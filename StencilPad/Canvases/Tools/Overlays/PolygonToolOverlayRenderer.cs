790f27e5ac8c86777a8512fc152ba6d1576e0bb4
Richard Possnett <95437308+dbb789@users.noreply.github.com>
2026-08-02 12:56:48 +0100

    Skia polygon overlay renderer

--- StencilPad/Canvases/Tools/Overlays/PolygonToolOverlayRenderer.cs
+++ StencilPad/Canvases/Tools/Overlays/PolygonToolOverlayRenderer.cs
@@ -1,5 +1,3 @@
-using Avalonia.Media;
-using Avalonia.Skia;
 using SkiaSharp;
 using StencilPad.Models;
 using StencilPad.Spatial;
@@ -24,26 +22,47 @@ public class PolygonToolOverlayRenderer : IToolOverlayRenderer
         }
     }
 
-    private static SKPaint EdgeOverlayPaint = new SKPaint
+    private class RenderedGeometry : IDisposable
+    {
+        public SKPath EdgeOverlayPath = new();
+        public SKPath ControlStemPath = new();
+        public SKMatrix Matrix = SKMatrix.Identity;
+
+        public void Reset()
+        {
+            EdgeOverlayPath.Reset();
+            ControlStemPath.Reset();
+        }
+
+        public void Dispose()
+        {
+            EdgeOverlayPath.Dispose();
+            ControlStemPath.Dispose();
+        }
+    }
+
+    private static readonly SKPaint EdgeOverlayPaint = new SKPaint
     {
         Color = new SKColor(0, 0, 255, 128),
-        StrokeWidth = 0.3f,
-        IsStroke = true
+        StrokeWidth = 2,
+        Style = SKPaintStyle.Stroke,
+        IsAntialias = true,
+        IsDither = true
     };
 
-    private static SKPaint ControlStemPaint = new SKPaint
+    private static readonly SKPaint ControlStemPaint = new SKPaint
     {
         Color = new SKColor(0, 200, 0, 128),
         StrokeWidth = 0.2f,
-        IsStroke = true
+        Style = SKPaintStyle.Stroke,
+        IsAntialias = true,
+        IsDither = true
     };
 
     private readonly IPolygonSheetElement _element;
-    private readonly StreamGeometryWalker _walker;
+    private readonly SKPathGeometryWalker _walker;
 
-    private Transform? _transform;
-    private StreamGeometry? _edgeOverlayGeometry;
-    private StreamGeometry? _controlStemGeometry;
+    private TripleBuffer<RenderedGeometry> _renderedGeometry;
     private bool _geometryDirty;
     
     public event Action? RendererDirty;
@@ -57,13 +76,14 @@ public class PolygonToolOverlayRenderer : IToolOverlayRenderer
         _element.TransformChanged += TransformChanged;
         
         _walker = new();
+        _renderedGeometry = new();
         
         foreach (var polygon in _element.PolygonSet)
         {
             polygon.GeometryChanged += MarkGeometryDirty;
         }
 
-        _transform = _element.Transform.CreateGroupTransform();
+        _geometryDirty = true;
 
         RebuildGeometry();
         _geometryDirty = false;
@@ -80,6 +100,8 @@ public class PolygonToolOverlayRenderer : IToolOverlayRenderer
         _element.PolygonSet.PolygonRemoved -= PolygonRemoved;
         _element.PolygonSet.HandleSource.HandleSelectionChanged -= SelectionChanged;
         _element.TransformChanged -= TransformChanged;
+
+        _renderedGeometry.Dispose();
     }
 
     private void PolygonAdded(EditablePolygon polygon)
@@ -113,101 +135,98 @@ public class PolygonToolOverlayRenderer : IToolOverlayRenderer
 
     private void TransformChanged(ISheetElement element)
     {
-        _transform = _element.Transform.CreateGroupTransform();
-        
-        InvokeRendererDirty();
-    }
-    
-    private void RebuildGeometry()
-    {
-        var polygonList = _element.PolygonSet;
-
-        _edgeOverlayGeometry = new StreamGeometry();
-
-        using (var ctx = _edgeOverlayGeometry.Open())
-        {
-            ctx.SetFillRule(FillRule.EvenOdd);
-
-            _walker.Context = ctx;
-            
-            foreach (var polygon in polygonList)
-            {
-                foreach (var edgeIndex in polygon.GetSelectedEdges())
-                {
-                    polygon.Resolver.WalkEdge(_walker, edgeIndex);
-                }
-            }
-        }
-
-        _controlStemGeometry = new StreamGeometry();
-
-        using (var ctx = _controlStemGeometry.Open())
-        {
-            ctx.SetFillRule(FillRule.EvenOdd);
-
-            foreach (var polygon in polygonList)
-            {
-                for (int i = 0; i < polygon.Edges.Count; i++)
-                {
-                    var edge = polygon.Edges[i];
-
-                    if (edge.Type == EdgeType.Bezier)
-                    {
-                        var vertexBegin = polygon.Vertices[i].Position;
-                        var controlBegin = vertexBegin + edge.ControlBeginOffset;
-
-                        ctx.BeginFigure(vertexBegin.Millimeters, isFilled: false);
-                        ctx.LineTo(controlBegin.Millimeters, isStroked: true);
-                        ctx.EndFigure(isClosed: false);
-
-                        var vertexEnd = polygon.Vertices.At(i + 1).Position;
-                        var controlEnd = vertexEnd + edge.ControlEndOffset;
-
-                        ctx.BeginFigure(vertexEnd.Millimeters, isFilled: false);
-                        ctx.LineTo(controlEnd.Millimeters, isStroked: true);
-                        ctx.EndFigure(isClosed: false);
-                    }
-                }
-            }
-        }
-    }
-
-    public void Render(DrawingContext dc)
-    {
-        // if (_geometryDirty)
-        // {
-        //     _geometryDirty = false;
-        //     RebuildGeometry();
-        // }
-
-        // if (_transform is null)
-        // {
-        //     return;
-        // }
-        
-        // using var state = dc.PushTransform(_transform.Value);
-        
-        // if (_edgeOverlayGeometry is not null)
-        // {
-        //     dc.DrawGeometry(Brushes.Transparent,
-        //                     _edgeOverlayPen,
-        //                     _edgeOverlayGeometry);
-        // }
-
-        // if (_controlStemGeometry is not null)
-        // {
-        //     dc.DrawGeometry(Brushes.Transparent,
-        //                     _controlStemPen,
-        //                     _controlStemGeometry);
-        // }
+        MarkGeometryDirty();
     }
 
     public void PreRender()
     {
+        if (_geometryDirty)
+        {
+            _geometryDirty = false;
+            RebuildGeometry();
+        }
     }
-    
+
     public void Render(SKCanvas canvas, GRContext? context)
     {
+        using var geometryHandle = _renderedGeometry.TryRead();
+
+        if (!geometryHandle.IsValid)
+        {
+            return;
+        }
+
+        var geometry = geometryHandle.Buffer;
+
+        canvas.Save();
+        canvas.SetMatrix(SKMatrix.Concat(canvas.TotalMatrix, geometry.Matrix));
+
+        if (!geometry.EdgeOverlayPath.IsEmpty)
+        {
+            canvas.DrawPath(geometry.EdgeOverlayPath, EdgeOverlayPaint);
+        }
+
+        if (!geometry.ControlStemPath.IsEmpty)
+        {
+            canvas.DrawPath(geometry.ControlStemPath, ControlStemPaint);
+        }
+
+        canvas.Restore();
+    }
+
+    private void RebuildGeometry()
+    {
+        using var geometryHandle = _renderedGeometry.TryWrite();
+
+        if (!geometryHandle.IsValid)
+        {
+            return;
+        }
+
+        var geometry = geometryHandle.Buffer;
+
+        geometry.Reset();
+        geometry.Matrix = _element.Transform.CreateMatrix();
+
+        var polygonList = _element.PolygonSet;
+
+        _walker.Path = geometry.EdgeOverlayPath;
+
+        foreach (var polygon in polygonList)
+        {
+            foreach (var edgeIndex in polygon.GetSelectedEdges())
+            {
+                polygon.Resolver.WalkEdge(_walker, edgeIndex);
+            }
+        }
+
+        foreach (var polygon in polygonList)
+        {
+            for (int i = 0; i < polygon.Edges.Count; i++)
+            {
+                var edge = polygon.Edges[i];
+
+                if (edge.Type == EdgeType.Bezier)
+                {
+                    var vertexBegin = polygon.Vertices[i].Position;
+                    var controlBegin = vertexBegin + edge.ControlBeginOffset;
+
+                    geometry.ControlStemPath.MoveTo(Point(vertexBegin));
+                    geometry.ControlStemPath.LineTo(Point(controlBegin));
+
+                    var vertexEnd = polygon.Vertices.At(i + 1).Position;
+                    var controlEnd = vertexEnd + edge.ControlEndOffset;
+
+                    geometry.ControlStemPath.MoveTo(Point(vertexEnd));
+                    geometry.ControlStemPath.LineTo(Point(controlEnd));
+                }
+            }
+        }
+    }
+
+    private static SKPoint Point(Unit2D point)
+    {
+        return new SKPoint((float)point.X.Millimeters, (float)point.Y.Millimeters);
     }
     
     private void InvokeRendererDirty()
