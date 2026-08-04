6b65be64ed6f033aa095a3ca4d314fd1db7fcd24
Richard Possnett <95437308+dbb789@users.noreply.github.com>
2026-08-04 11:44:40 +0100

    Various

--- StencilPad/UI/Widgets/UnitTypeItem.cs
+++ StencilPad/UI/Widgets/UnitTypeItem.cs
@@ -6,4 +6,9 @@ public class UnitTypeItem
 {
     public UnitType Value { get; set; }
     public string Description { get; set; } = "";
+
+    public override string ToString()
+    {
+        return Description;
+    }
 }
