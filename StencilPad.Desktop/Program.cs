9dc82b2f4db6f7c9dd3b9842ea1b35efc98e9f86
Richard Possnett <95437308+dbb789@users.noreply.github.com>
2026-08-14 17:57:24 +0100

    Test NSApplication.Init() in Main()

--- StencilPad.Desktop/Program.cs
+++ StencilPad.Desktop/Program.cs
@@ -1,5 +1,9 @@
 ﻿using Avalonia;
 
+#if SP_OSX
+using AppKit;
+#endif
+
 namespace StencilPad.Desktop;
 
 internal sealed class Program
@@ -8,9 +12,15 @@ internal sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
-    
+    public static void Main(string[] args)
+    {
+#if SP_OSX
+        NSApplication.Init();
+#endif
+
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
+
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
     => AppBuilder.Configure<App>()
