f6a1e217bc712a626f03acbcc72eae07f506a6d4
Richard Possnett <95437308+dbb789@users.noreply.github.com>
2026-08-27 15:13:35 +0100

    More line style options

--- StencilPad/Services/LineStyleResourceLibrary.cs
+++ StencilPad/Services/LineStyleResourceLibrary.cs
@@ -16,6 +16,6 @@ public static class LineStyleResourceLibrary
             new LineStyle(Unit.FromMillimeters(3), Unit.FromMillimeters(1)),
             new LineStyle(Unit.FromMillimeters(3), Unit.FromMillimeters(3)),
             new LineStyle(Unit.FromMillimeters(1), Unit.FromMillimeters(2)),
-            new LineStyle(Unit.FromMillimeters(1), Unit.FromMillimeters(3)),
+            new LineStyle(Unit.FromMillimeters(0.5), Unit.FromMillimeters(0.5), Unit.FromMillimeters(1), Unit.FromMillimeters(0.5))
         ];
 }
