9169f6421c678ff2d3fa242f2ab4956aa05730c8
Richard Possnett <95437308+dbb789@users.noreply.github.com>
2026-08-27 10:17:35 +0100

    Typo

--- StencilPad/Spatial/Unit.cs
+++ StencilPad/Spatial/Unit.cs
@@ -184,7 +184,7 @@ public readonly record struct Unit
     public static Unit operator /(Unit u, Fraction scalar) => new((u._value * scalar.Denominator) / scalar.Numerator);
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static Unit operator /(Fraction scalar, Unit u) => u / scalar;
+    public static Unit operator /(Fraction scalar, Unit u) => scalar / u;
 
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Unit operator /(Unit u, decimal scalar) => new(u._value / scalar);
